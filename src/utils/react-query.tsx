49340abe9472737cf5b061409f41be9671af86af
SafeDuck <schopra579@gmail.com>
2024-09-08 22:32:43 -0700

    Convert QueryClientProvider to TS

---
+++ src/utils/react-query.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60000,
+      gcTime: 10 * 60 * 1000,
+    },
+  },
+});
+
+export const ReactQueryClientProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
