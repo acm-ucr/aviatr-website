0be69d2562097fd8fd4b4629c33252af09d6a595
Naya Singhania <naya.singhania@gmail.com>
2024-09-29 17:56:02 -0700

    fixed style

--- src/app/components/learn-more.tsx
+++ src/app/components/learn-more.tsx
@@ -1,24 +1,21 @@
 //color should be passed into the component as a hexcode. ex #ADD8E6
-const LearnMore = (props: {color: string}) => {
-    return (
-         <button className="flex items-center bg-transparent hover:bg-gray-200 text-black py-1 px-1 text-xs rounded-lg">
-            <span>LEARN MORE</span>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="w-6 h-6 ml-2 rounded-full"
-                style={{ backgroundColor: props.color }}
-            >
-                <circle cx="12" cy="12" r="12" fill={props.color} />
-                <path
-                    d="M6 12h12M12 6l6 6-6 6"
-                    className="text-black"
-                />
-            </svg>
-        </button>
-    )
-}
+const LearnMore = (props: { color: string }) => {
+  return (
+    <button className="flex items-center rounded-lg bg-transparent px-1 py-1 text-xs text-black hover:bg-gray-200">
+      <span>LEARN MORE</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="ml-2 h-6 w-6 rounded-full"
+        style={{ backgroundColor: props.color }}
+      >
+        <circle cx="12" cy="12" r="12" fill={props.color} />
+        <path d="M6 12h12M12 6l6 6-6 6" className="text-black" />
+      </svg>
+    </button>
+  );
+};
 
 export default LearnMore;
