b951a9bd575d23d2a1a3dfb6e256776d1356201a
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-10-09 12:08:04 -0700

    Revert "clean up"
    This reverts commit d29178e706e01794a8a4d6c0fe4a647429fc2dee.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,11 @@
+import Example from "@/components/Example";
+
 const Projects = () => {
-  return <div className="justify-center">Projects</div>;
+  return (
+    <div className="justify-center">
+      Projects
+      <Example />
+    </div>
+  );
 };
 export default Projects;
