5ad37c2187e748fb77e94b62bc45cb82502e21c4
Akshay Jayaram <akshayj1814@gmail.com>
2024-10-11 13:43:57 -0700

    Add spacing around the text component

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -18,13 +18,14 @@ const NotFound = () => {
         <div className="font-bungee-hairline text-6xl font-normal leading-tight text-white sm:text-7xl md:text-8xl">
           404
         </div>
-        <div className="mb-6 w-1/2 border-b-2 border-white p-1"></div>
-        <div className="font-jost text-2xl font-normal leading-tight text-white sm:text-3xl">
+        <div className="mb-6 w-1/4 border-b-2 border-white p-1"></div>
+        <div className="font-jost mt-0 text-3xl font-normal leading-[43.35px] text-white sm:text-xl">
           PAGE NOT FOUND
         </div>
-        <div className="font-jost max-w-md text-base font-normal leading-normal text-white sm:text-lg">
-          Sorry, the page you're looking for cannot be found!
-          <br /> Go back or return home.
+        <div className="font-jost mb-4 mt-4 max-w-md text-lg font-normal leading-[28.9px] text-white sm:text-base">
+          Sorry, the page you're looking for cannot
+          <br />
+          be found! Go back or return home.
         </div>
         <button className="mt-4 rounded-full bg-blue-300 px-6 py-2 transition-colors hover:bg-blue-400">
           BACK HOME
