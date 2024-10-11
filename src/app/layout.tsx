7e30f068a6508dd168461f2d49619753505dfdb3
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-10-11 16:07:41 -0700

    font and colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,28 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Jost, Metrophobic, Bungee_Hairline } from "next/font/google";
 import { ReactQueryClientProvider } from "@/utils/react-query";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+const jost = Jost({
+  subsets: ["latin"],
+  variable: "--font-jost",
+  display: "swap",
+});
+
+const metrophobic = Metrophobic({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-metrophobic",
+  display: "swap",
+});
+
+const bungee_hairline = Bungee_Hairline({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-bungee-hairline",
+  display: "swap",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -17,7 +36,9 @@ type LayoutProps = {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body className={`bg-black ${inter.className}`}>
+      <body
+        className={`bg-black ${jost.variable} ${metrophobic.variable} ${bungee_hairline.variable}`}
+      >
         <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
         <Footer />
       </body>
