341d1a6ec3c3e31bed1b41f5662458d3be3a9fc3
Yoshinao <yoshinaoshimono@gmail.com>
2024-10-14 11:45:40 -0700

    board member component

--- src/components/about/BoardMember.tsx
+++ src/components/about/BoardMember.tsx
@@ -1,46 +1,27 @@
 import React from "react";
-import { CSSProperties } from "react";
 
-const BoardMember = ({ name, position, image }: {name: any, position:any, image: any}) => {
-    return (
-      <div style={styles.container}>
-        <img src={image} alt={name + "'s profile image"} style={styles.image} />
-        <div>
-          <div style={styles.name}>{name}</div>
-          <div style={styles.position}>{position}</div>
-        </div>
+const BoardMember = ({
+  name,
+  position,
+  image,
+}: {
+  name: string;
+  position: string;
+  image: string;
+}) => {
+  return (
+    <div className="flex flex-col items-center justify-center w-64 p-4 rounded-lg text-center shadow-lg">
+      <img
+        src={image}
+        alt={`${name}'s profile image`}
+        className="w-48 h-56 rounded-lg mb-5 object-cover"
+      />
+      <div>
+        <div className="text-xl font-bold">{name}</div>
+        <div className="text-lg text-gray-600">{position}</div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-
-  const styles: { [key: string]: CSSProperties } = {
-    container: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",  
-        justifyContent: "center", 
-        width: "250px",
-        padding: "10px",
-        borderRadius: "10px",
-        textAlign: "center",
-    },
-    image: {
-      width: "189.07px",
-      height: "223.87px",
-      borderRadius: "15%",
-      marginBottom: "20px",
-    },
-    name: {
-      fontSize: "22px",
-      fontWeight: "bold",
-      alignItems: "center",
-    },
-    position: {
-      fontSize: "18px",
-      color: "#555",
-      alignItems: "center",
-    },
-  };
-  
-  export default BoardMember;
+export default BoardMember;
