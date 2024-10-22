2cbb2598f2ceee672ff04710a1cd078e0fdf113c
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-10-22 13:57:30 -0700

    edits

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,14 +9,16 @@ const Footer = () => {
     <div>
       <hr className="mx-[3%] mb-[5%] mt-2 border-t-2 xl:mb-[3%]"></hr>
       <div className="sm:grid sm:grid-cols-2">
-        <div className="mx-[6%]cflex justify-between whitespace-nowrap font-jost text-white sm:mx-0 sm:ml-[12%] sm:grid sm:grid-cols-3 lg:font-normal">
+        <div className="mx-[6%] flex justify-between whitespace-nowrap font-jost text-white sm:mx-0 sm:ml-[12%] sm:grid sm:grid-cols-3">
           <div>
             <p className="pb-1.5 text-sm md:pb-2 md:text-xl lg:pb-3 lg:text-3xl">
               PROJECTS
             </p>
-            <p className="text-xs md:text-sm lg:text-lg">PROJECT PUFFIN</p>
-            <p className="text-xs md:text-sm lg:text-lg">HUMMINGBIRD</p>
-            <p className="text-xs md:text-sm lg:text-lg">EAGLE</p>
+            <div className="text-xs md:text-sm lg:text-lg">
+              <p>PROJECT PUFFIN</p>
+              <p>HUMMINGBIRD</p>
+              <p>EAGLE</p>
+            </div>
           </div>
           <div>
             <p className="pb-1.5 text-sm md:pb-2 md:text-xl lg:pb-3 lg:text-3xl">
