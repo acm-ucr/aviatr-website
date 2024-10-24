854ccb68148078698c36cc4355b145f424dc2022
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-10-24 12:23:07 -0700

    edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Title from "@/components/Title";
 import HummingbirdHomepage from "@/components/home/HummingbirdHomepage";
 import AboutAviat from "@/components/home/AboutAviat";
 import Eagle from "@/components/home/Eagle";
@@ -8,7 +7,6 @@ const Home = () => {
   return (
     <div className="justify-center">
       <AboutAviat />
-      <Title title={"Thomas"} />
       <HummingbirdHomepage />
       <Eagle />
     </div>
