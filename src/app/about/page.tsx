7fc8b1a07fb33a0e2fec72fa94afcf972d5cae68
Kevin Loritsch <kloritsch58@gmail.com>
2024-10-25 15:47:36 -0700

    Fix About

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import BoardMemberSection from "../../components/about/BoardMemberSection";
 
 const About = () => {
-  return <div>
-    <BoardMemberSection />;
-  </div>
+  return (
+    <div>
+      <BoardMemberSection />;
+    </div>
+  );
 };
 export default About;
