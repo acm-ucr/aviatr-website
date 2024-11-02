f5bf88a6937af436ecc419c806e74f528f189227
Abhinav Sharma <abhiinaav820@gmail.com>
2024-11-02 16:52:47 -0700

    Added percents to RecurringLogo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import BoardMemberSection from "../../components/about/BoardMemberSection";
 import WhoWeAre from "../../components/about/WhoWeAre";
 import WhatWeDo from "../../components/about/WhatWeDo";
+import RecurringLogo from "@/components/RecurringLogo";
 
 const Events = () => {
   return (
-    <div className="flex flex-col gap-24">
-      <WhoWeAre />
-      <WhatWeDo />
-      <BoardMemberSection />
+    <div>
+      <div className="p-20">
+        <RecurringLogo />
+      </div>
+      <div className="flex flex-col gap-24">
+        <WhoWeAre />
+        <WhatWeDo />
+        <BoardMemberSection />
+      </div>
     </div>
   );
 };
--- src/components/RecurringLogo.tsx
+++ src/components/RecurringLogo.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import RecurringImg from "@/images/Logo.webp";
 
 const RecurringLogo = () => {
   return (
     <div className="flex justify-center">
-      <div className="flex h-[376px] w-[687px] items-center justify-center rounded-full bg-white p-4">
+      <div className="flex h-96 w-auto items-center justify-center rounded-full bg-white p-4">
         <div className="flex items-center justify-center">
           <Image
-            src="\Images\Logo.webp"
+            src={RecurringImg}
             alt="RecurringLogo"
             width={687}
             height={376}
