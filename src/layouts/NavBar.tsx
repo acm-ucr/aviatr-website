08cae042eb207bf87ed2b8fa026611d3bd46b1d4
Aryan Goel <52222311+agoel27@users.noreply.github.com>
2024-11-14 06:02:56 +0000

    Formatted code

--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -20,36 +20,50 @@ const Navbar = () => {
 
   return (
     <div className="sticky top-0 z-50">
-    <div
-      className={`flex w-full items-center ${isHomePage ? "bg-transparent" : "bg-aviatr-blue-400"}`}
-    >
-      <div className="w-1/6 ml-5 md:ml-10">
-        <Link href={"/"}>
-          <Image src={Logo} alt="logo" />
-        </Link>
+      <div
+        className={`flex w-full items-center ${isHomePage ? "bg-transparent" : "bg-aviatr-blue-400"}`}
+      >
+        <div className="ml-5 w-1/6 md:ml-10">
+          <Link href={"/"}>
+            <Image src={Logo} alt="logo" />
+          </Link>
+        </div>
+        <div className="flex w-5/6 justify-end">
+          <div className="hidden grid-cols-4 divide-x-2 divide-black font-jost text-xs text-black md:grid lg:text-sm xl:text-base 2xl:text-lg">
+            {items.map((item, index) => (
+              <div
+                key={index}
+                className="content-center px-6 text-center lg:px-8 2xl:px-12"
+              >
+                <Link
+                  href={item.link}
+                  className={`${item.name === "JOIN" ? "rounded-full bg-aviatr-blue-100 px-6 py-1 lg:px-8 xl:px-10 xl:py-2" : ""}`}
+                >
+                  {item.name}
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div onClick={handleNav}>
+          <Menu className="mr-2 hover:cursor-pointer md:hidden" />
+        </div>
       </div>
-      <div className="flex justify-end w-5/6">
-        <div className="hidden grid-cols-4 divide-x-2 divide-black font-jost text-black text-xs lg:text-sm xl:text-base 2xl:text-lg md:grid">
+      <div
+        className={`fixed grid w-full grid-rows-4 justify-items-center gap-y-2 bg-aviatr-blue-400 pb-2 font-jost text-base md:hidden ${nav ? "" : "hidden"}`}
+      >
         {items.map((item, index) => (
-            <div key={index} className="text-center content-center px-6 lg:px-8 2xl:px-12">
-              <Link href={item.link} className={`${item.name === "JOIN" ? "rounded-full bg-aviatr-blue-100 px-6 lg:px-8 xl:px-10 py-1 xl:py-2" : ""}`}>{item.name}</Link>
-            </div>
+          <div key={index}>
+            <Link
+              className="border-b border-black"
+              onClick={handleNav}
+              href={item.link}
+            >
+              {item.name}
+            </Link>
+          </div>
         ))}
-        </div>
       </div>
-      <div onClick={handleNav} >
-        <Menu
-          className="mr-2 md:hidden hover:cursor-pointer"
-        />
-      </div>
-    </div>
-    <div className={`fixed md:hidden w-full pb-2 grid grid-rows-4 gap-y-2 text-base font-jost justify-items-center bg-aviatr-blue-400 ${nav ? "" : "hidden"}`}>
-      {items.map((item, index) => (
-        <div key={index}>
-          <Link className=" border-b border-black " onClick={handleNav} href={item.link}>{item.name}</Link>
-        </div>
-      ))}
-    </div>
     </div>
   );
 };
