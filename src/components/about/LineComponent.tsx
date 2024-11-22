a7d02bd5b2e8dbd56fc4ca51a12dec9e48ecf432
Thomas Acheampong <thomasacheampong@Thomass-MacBook-Air.local>
2024-11-22 02:16:54 -0800

    added image animations on scroll for eagle page

---
+++ src/components/about/LineComponent.tsx
@@ -0,0 +1,27 @@
+// Divider.js
+
+const Divider = ({
+    color = "#ccc",
+    gradient = null,
+    thickness = "1px",
+    width = "100%",
+    margin = "1rem 0",
+    className = "",
+  }) => {
+    return (
+      <hr
+        style={{
+          backgroundColor: gradient ? `linear-gradient(${gradient})` : color,
+          height: thickness,
+          width: width,
+          margin: margin,
+          border: "none",
+        }}
+        className={className}
+      />
+    );
+  };
+  
+  export default Divider;
+  
+  
--- src/components/projects/Eagle.tsx
+++ src/components/projects/Eagle.tsx
@@ -1,11 +1,22 @@
+"use client"
 import Image from "next/image";
 import EagleImg1 from "@/../public/projects/Eagle1.webp";
 import EagleImg2 from "@/../public/projects/Eagle2.webp";
 import EagleImg3 from "@/../public/projects/Eagle3.webp";
 import ShiftedLineTitle from "@/components/ShiftedLineTitle";
 import Link from "next/link";
+import { motion, useInView } from "framer-motion" 
+import {useRef} from "react";
 
 const Eagle = () => {
+  const ref = useRef(null);
+  const isInView = useInView (ref, { once : false});
+
+  const imageVariants= {
+    hidden : { opacity: 0, scale: 0.9, y: 50},
+    visible: { opacity : 1,  scale: 1, y: 0},
+  };
+
   return (
     <div className="mx-[8%] my-[5%] font-jost text-white">
       <div className="mb-[8%] sm:mb-[6%]">
@@ -17,29 +28,53 @@ const Eagle = () => {
         />
       </div>
 
-      <div>
-        <div className="mb-[5%] grid grid-cols-3 gap-2 sm:gap-4">
-          <div className="col-span-2">
+      <div ref={ref}>
+        <motion.div 
+         className="mb-[5%] grid grid-cols-3 gap-2 sm:gap-4"
+         initial="hidden"
+         animate = {isInView ? "visible": "hidden"}
+         variants = {{
+          hidden: {},
+          visible: { transition : { staggerchildren: 0.2}},
+         }}
+         >
+          <motion.div 
+          className="col-span-2"
+          variants= {imageVariants}
+          transition={{duration:0.8}}
+          >
             <Image
               src={EagleImg1}
               alt="EagleImg1"
               className="h-full w-full object-cover"
             />
-          </div>
+          </motion.div>
+
           <div className="grid grid-rows-2 gap-2 sm:gap-4">
-            <Image
+          <motion.div
+              className="h-full w-full"
+              variants={imageVariants}
+              transition={{ duration: 0.8, delay: 0.2 }}
+              >
+            <Image 
               src={EagleImg2}
               alt="EagleImg2"
               className="h-full w-full object-cover"
             />
+            </motion.div>
+            <motion.div
+              className="h-full w-full"
+              variants={imageVariants}
+              transition={{ duration: 0.8, delay: 0.4 }}
+            >
             <Image
               src={EagleImg3}
               alt="EagleImg3"
               className="h-full w-full object-cover"
             />
-          </div>
+          </motion.div>
         </div>
-
+        </motion.div>
         <p className="text-md mx-[2%] mb-[5%] text-center font-jost sm:mb-0 sm:text-justify sm:text-xl">
           The Eagle Project is our competitive team dedicated to designing
           Unmanned Aerial Vehicles (UAVs) for global competitions. Participation
