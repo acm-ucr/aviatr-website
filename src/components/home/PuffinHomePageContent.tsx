14ba53da476003c2173b347ff98743212346b1d1
Yoshinao <yoshinaoshimono@gmail.com>
2024-11-22 01:13:50 -0800

    changed project puffin title

--- src/components/home/PuffinHomePageContent.tsx
+++ src/components/home/PuffinHomePageContent.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import PuffinImg from "@/../public/home/ProjectPuffin.webp";
 import LearnMoreButton from "./LearnMoreButton";
+import ShiftedLineTitle from "@/components/ShiftedLineTitle";
 
 const ProjectPuffin = () => {
   return (
@@ -20,17 +21,23 @@ const ProjectPuffin = () => {
 
       {/* Content */}
       <div className="relative z-10 ml-[-60%] max-w-lg p-8">
-        <h1 className="mb-4 font-metrophobic text-6xl font-bold">
-          Project Puffin
-        </h1>
-        <div className="w-1/1.5 mb-6 ml-20 border-b-2 border-white p-1"></div>
-        <p className="mb-6 font-jost text-xl leading-relaxed">
-          Project Puffin aims at giving those interested in but without a
-          background in aeronautics the general know-how of drones. By the end
-          of the 2 unit class, students will be able to assemble their own
-          drones and understand the parts and basic theory behind them.
-        </p>
-        <LearnMoreButton color="bg-aviatr-pink-100" link="/projects" />
+        <div className="mb-[7%] mr-[4%] mt-[6%] sm:mr-0 md:mb-[12%] lg:mr-[14%] lg:mt-0">
+          <ShiftedLineTitle
+            title="Project Puffin"
+            width="md:w-[90%]"
+            textSize="lg:text-5xl"
+            lineMarginShift="ml-[30%]"
+          />
+        </div>
+        <div className="ml-[7%]">
+          <p className="mb-6 font-jost text-xl leading-relaxed">
+            Project Puffin aims at giving those interested in but without a
+            background in aeronautics the general know-how of drones. By the end
+            of the 2 unit class, students will be able to assemble their own
+            drones and understand the parts and basic theory behind them.
+          </p>
+          <LearnMoreButton color="bg-aviatr-pink-100" link="/projects" />
+        </div>
       </div>
     </div>
   );
