49b0840de6e2399ef56cb4e4d1ea60302875d021
Kevin Loritsch <kloritsch58@gmail.com>
2024-11-22 15:56:03 -0800

    Fix 404 Animate

--- src/components/not_found/NotFound.tsx
+++ src/components/not_found/NotFound.tsx
@@ -37,50 +37,50 @@ const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-black p-8 md:flex-row">
       <div className="z-10 mb-8 md:mb-0 md:mr-32">
-      <motion.div
-        className="sm:mb-[3%]"
-        variants={animationX}
-        transition={{ ...transition }}
-        initial="hidden"
-        whileInView="show"
-      >
-        <Image
-          src={drone}
-          alt="Drone illustration"
-          width={550}
-          height={406}
-        />
+        <motion.div
+          className="sm:mb-[3%]"
+          variants={animationX}
+          transition={{ ...transition }}
+          initial="hidden"
+          whileInView="show"
+        >
+          <Image
+            src={drone}
+            alt="Drone illustration"
+            width={550}
+            height={406}
+          />
         </motion.div>
       </div>
 
       <div className="flex flex-col items-center gap-2 text-center">
-      <motion.div
-        className="sm:mb-[3%]"
-        variants={animationScale}
-        transition={{ ...transition }}
-        initial="hidden"
-        whileInView="show"
-      >
-        <div className="font-outline-8 mb-0 font-bungee-hairline text-8xl font-normal leading-tight text-white">
-          404
-        </div>
+        <motion.div
+          className="sm:mb-[3%]"
+          variants={animationScale}
+          transition={{ ...transition }}
+          initial="hidden"
+          whileInView="show"
+        >
+          <div className="font-outline-8 mb-0 font-bungee-hairline text-8xl font-normal leading-tight text-white">
+            404
+          </div>
         </motion.div>
         <div className="mb-6 w-1/3 border-b-2 border-white p-1"></div>
         <div className="mb-5 font-jost text-3xl font-normal text-white">
           PAGE NOT FOUND
         </div>
         <motion.div
-        className="sm:mb-[3%]"
-        variants={animationY}
-        transition={{ ...transition }}
-        initial="hidden"
-        whileInView="show"
-      >
-        <div className="mb-5 max-w-md font-jost text-xl font-normal text-white">
-          Sorry, the page you're looking for cannot
-          <br />
-          be found! Go back or return home.
-        </div>
+          className="sm:mb-[3%]"
+          variants={animationY}
+          transition={{ ...transition }}
+          initial="hidden"
+          whileInView="show"
+        >
+          <div className="mb-5 max-w-md font-jost text-xl font-normal text-white">
+            Sorry, the page you're looking for cannot
+            <br />
+            be found! Go back or return home.
+          </div>
         </motion.div>
         <Link
           href="/"
