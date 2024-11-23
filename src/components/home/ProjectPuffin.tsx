32db3433a051dc13a15c94098c19dc222a44f8d2
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-11-22 21:03:29 -0800

    homepage project puffin

--- src/components/home/ProjectPuffin.tsx
+++ src/components/home/ProjectPuffin.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import PuffinImg from "@/../public/home/ProjectPuffin.webp";
@@ -6,22 +7,10 @@ import ShiftedLineTitle from "@/components/ShiftedLineTitle";
 
 const ProjectPuffin = () => {
   return (
-    <div className="relative mt-[8%] flex items-center justify-center bg-black text-white lg:h-screen">
-      {/* Background Image */}
-      <div className="absolute inset-0">
-        <Image
-          src={PuffinImg}
-          alt="Project Puffin background image"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-          className="opacity-60"
-        />
-      </div>
-
+    <div className="relative mt-[12%] flex w-full flex-col justify-center text-white">
       {/* Content */}
-      <div className="relative z-10 max-w-lg p-8 md:ml-[-35%] xl:ml-[-50%]">
-        <div className="mb-[7%] mr-[4%] mt-[6%] sm:mr-0 md:mb-[12%] lg:mr-[14%] lg:mt-0">
+      <div className="left-[5%] z-10 max-w-lg p-8 md:absolute xl:left-[8%]">
+        <div className="mb-[7%] mr-[4%] sm:mr-0 md:mb-[12%] lg:mr-[14%] lg:mt-0">
           <ShiftedLineTitle
             title="Project Puffin"
             width="md:w-[90%]"
@@ -30,7 +19,7 @@ const ProjectPuffin = () => {
           />
         </div>
         <div className="ml-[7%]">
-          <p className="mb-6 font-jost text-xl leading-relaxed">
+          <p className="text-md mb-6 font-jost leading-relaxed sm:text-xl">
             Project Puffin aims at giving those interested in but without a
             background in aeronautics the general know-how of drones. By the end
             of the 2 unit class, students will be able to assemble their own
@@ -39,6 +28,13 @@ const ProjectPuffin = () => {
           <LearnMoreButton color="bg-aviatr-pink-100" link="/projects" />
         </div>
       </div>
+
+      {/* Background Image */}
+      <Image
+        src={PuffinImg}
+        alt="Project Puffin background image"
+        className="aspect-video w-full object-cover opacity-50 lg:opacity-60"
+      />
     </div>
   );
 };
