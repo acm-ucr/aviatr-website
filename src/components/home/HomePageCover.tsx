bae95b5ec7cdedea231735d061b29fdef5ee5d2f
Aryan Goel <52222311+agoel27@users.noreply.github.com>
2024-11-27 23:11:19 +0000

    Added fade down animation to landing image

--- src/components/home/HomePageCover.tsx
+++ src/components/home/HomePageCover.tsx
@@ -1,16 +1,36 @@
+"use client";
 import Image from "next/image";
 import Cover from "@/../public/home/homepagecover.webp";
+import { motion } from "motion/react";
+
+const animationY = {
+  hidden: { opacity: 0, y: -50 },
+  show: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+const transition = {
+  duration: 0.7,
+};
 
 const HomePageCover = () => {
   return (
-    <div className="relative">
+    <motion.div
+      variants={animationY}
+      transition={{ ...transition, delay: 0.2 }}
+      initial="hidden"
+      whileInView="show"
+      className="relative"
+    >
       <Image
         src={Cover}
         alt="Background image"
         className="h-auto w-full object-cover"
       />
       <div className="absolute bottom-0 left-0 right-0 h-[20%] bg-gradient-to-t from-black to-transparent"></div>
-    </div>
+    </motion.div>
   );
 };
 
