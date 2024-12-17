ac9c07f7bb4306d1ee0959502f5f8bec92787741
Kevin Loritsch <kloritsch58@gmail.com>
2024-12-17 15:31:50 -0800

    fix linting

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,23 +1,11 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import HummingBird from "@/components/projects/HummingBird";
 import Eagle from "@/components/projects/Eagle";
 import ProjectPuffin from "@/components/projects/ProjectPuffin";
 import DividingLine from "@/components/projects/DividingLine";
 
 const Projects = () => {
-  // useEffect(() => {
-  //   const hash = window.location.hash; // returns the hash in the url #..
-  //   if (hash) {
-  //     const section = document.querySelector(hash); // returns the first element identified by the hash, < id="#..">...</>
-  //     if (section instanceof HTMLElement) {
-  //       window.scrollTo({
-  //         top: section.offsetTop - 150,
-  //         behavior: "smooth",
-  //       });
-  //     }
-  //   }
-  // }, []);
 
   return (
     <div className="justify-center">
