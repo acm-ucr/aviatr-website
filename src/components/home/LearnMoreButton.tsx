c21d99409c8dc219e7fc88a4053c1529f40f8483
Mailyn <ngu.mailyn.yen@gmail.com>
2024-12-16 21:22:22 -0800

    learn more button links

--- src/components/home/LearnMoreButton.tsx
+++ src/components/home/LearnMoreButton.tsx
@@ -5,22 +5,20 @@ import { motion } from "motion/react";
 
 const LearnMoreButton = ({ color, link }: { color: string; link: string }) => {
   return (
-    <div className="flex items-center gap-2 md:gap-3">
-      <Link href={`${link}`}>
-        <motion.p
-          className="mr-2 whitespace-nowrap font-metrophobic text-xl text-white hover:underline md:text-2xl"
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          LEARN MORE
-        </motion.p>
-      </Link>
-      <div
-        className={`flex min-h-7 min-w-7 items-center justify-center rounded-full ${color}`}
+    <Link href={`${link}`}>
+      <motion.div
+        className="mr-2 flex flex-row gap-3 whitespace-nowrap font-metrophobic text-xl text-white hover:underline md:text-2xl"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
       >
-        <MoveRight color="black" size={24} className="w-[60%] md:w-full" />
-      </div>
-    </div>
+        LEARN MORE
+        <div
+          className={`flex h-7 min-h-7 w-7 min-w-7 items-center justify-center rounded-full md:h-8 md:w-8 ${color}`}
+        >
+          <MoveRight color="black" size={24} />
+        </div>
+      </motion.div>
+    </Link>
   );
 };
 
