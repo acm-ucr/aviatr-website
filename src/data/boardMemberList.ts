d726d9e4275d366bf6353bdb563a80eda306e33a
Mailyn <ngu.mailyn.yen@gmail.com>
2025-01-02 11:50:41 -0800

    added eric to board member

--- src/data/boardMemberList.ts
+++ src/data/boardMemberList.ts
@@ -5,6 +5,7 @@ import Evan from "@/../public/about/board_members/Evan_Goldman.webp";
 import Nicholas from "@/../public/about/board_members/Nicholas_Poon.webp";
 import Jonathan from "@/../public/about/board_members/Jonathan_Setiabudi.webp";
 import Wayman from "@/../public/about/board_members/Wayman_Mathis.webp";
+import Eric from "@/../public/about/board_members/Eric_Thueson.webp";
 
 const List = [
   {
@@ -77,6 +78,16 @@ const List = [
     major: "Electrical Engineering",
     description:
       "My name is Wayman Mathis, and I'm a 4th-year Electrical Engineering student at the University of California, Riverside. Currently, I'm expanding my skill set in UAV technology, applying my background in circuit design and power management to help advance our team's unmanned aerial systems. I'm always eager to tackle new challenges, pushing myself to find innovative solutions that build my expertise. In my free time, you'll often find me playing with my jazz combo or unwinding with friends.",
+  },
+  {
+    name: "Eric Thueson",
+    position: "President",
+    image: Eric.src,
+    hobby: "Aviation",
+    year: "Grad Student",
+    major: "Mechanical Engineering",
+    description:
+      "My name is Eric. I completed my undergrad in mechanical engineering in June of 2024. I started grad school in September of 2024. I wanted to join this organization to help bring more of an aerospace presence to UCR. The organization we are helping to mold brings together people from a variety of engineering disciplines and other areas. Each of us are learning various aspects about creating a drone that we intend to use in competition. I welcome those with a passion for aviation, and thank you for taking the time to check out our organization.",
   },
 ];
 
